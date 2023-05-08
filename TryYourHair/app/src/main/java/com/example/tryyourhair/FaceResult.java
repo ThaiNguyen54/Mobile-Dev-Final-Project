a8e63a94296272fd8c392a2ce161c8d532c4dfa5
ThaiNguyen54 <19522188@gm.uit.edu.vn>
2023-05-08 16:02:40 +0700

    modify animation

--- TryYourHair/app/src/main/java/com/example/tryyourhair/FaceResult.java
+++ TryYourHair/app/src/main/java/com/example/tryyourhair/FaceResult.java
@@ -3,13 +3,19 @@ package com.example.tryyourhair;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.tryyourhair.MainActivity;
+
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -33,6 +39,8 @@ import java.util.List;
 
 import ErrorDialog.NoFaceDialog;
 
+
+
 public class FaceResult extends AppCompatActivity {
 
     ImageView FaceResultView;
@@ -40,6 +48,9 @@ public class FaceResult extends AppCompatActivity {
     FaceDetector faceDetector;
 
     LottieAnimationView ScanEffectView;
+
+    Button BtnRetake;
+    Button BtnConfirm;
 
 
     //This factor is used to make the detecting image smaller, to make the process faster
@@ -54,6 +65,12 @@ public class FaceResult extends AppCompatActivity {
 
         FaceResultView = findViewById(R.id.face_img_view);
         ScanEffectView = findViewById(R.id.scan_effect);
+        BtnRetake = findViewById(R.id.btn_retake);
+        BtnConfirm = findViewById(R.id.btn_confirm);
+
+        BtnRetake.setVisibility(View.INVISIBLE);
+        BtnConfirm.setVisibility(View.INVISIBLE);
+
 
         // Receive bitmap from another activity
         byte[] byteArray = getIntent().getByteArrayExtra("userface");
@@ -62,83 +79,136 @@ public class FaceResult extends AppCompatActivity {
         // Display the bitmap
         FaceResultView.setImageBitmap(bitmap);
 
-        // Init FaceDetector Object
-        FaceDetectorOptions realTimeFdo = new FaceDetectorOptions.Builder()
-                .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
-                .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
-                .build();
+        Thread DetectFaceThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
 
-        faceDetector = FaceDetection.getClient(realTimeFdo);
+                // Init FaceDetector Object
+                FaceDetectorOptions realTimeFdo = new FaceDetectorOptions.Builder()
+                        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+                        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                        .build();
 
-        // Create a smaller bitmap to process faster
-        Bitmap smallerBitmap = Bitmap.createScaledBitmap(
-                bitmap,
-                bitmap.getWidth() / SCALING_FACTOR,
-                bitmap.getHeight() / SCALING_FACTOR,
-                false);
+                faceDetector = FaceDetection.getClient(realTimeFdo);
+
+                // Create a smaller bitmap to process faster
+                Bitmap smallerBitmap = Bitmap.createScaledBitmap(
+                        bitmap,
+                        bitmap.getWidth() / SCALING_FACTOR,
+                        bitmap.getHeight() / SCALING_FACTOR,
+                        false);
 
 
-        // Create an input image for detecting face process from the smaller bitmap
-        InputImage inputImage = InputImage.fromBitmap(smallerBitmap, 0);
+                // Create an input image for detecting face process from the smaller bitmap
+                InputImage inputImage = InputImage.fromBitmap(smallerBitmap, 0);
 
-        // Start the detection process
-        faceDetector.process(inputImage)
-                .addOnSuccessListener(new OnSuccessListener<List<Face>>() {
-                    @Override
-                    public void onSuccess(List<Face> faces) {
+                // Start the detection process
+                faceDetector.process(inputImage)
+                        .addOnSuccessListener(new OnSuccessListener<List<Face>>() {
+                            @Override
+                            public void onSuccess(List<Face> faces) {
+                                // After detecting successfully, we need to disable the animation view
+                                ScanEffectView.setVisibility(View.GONE);
+                                BtnConfirm.setVisibility(View.VISIBLE);
+                                BtnRetake.setVisibility(View.VISIBLE);
+                                Rect rect = null;
+                                if (faces.size() == 0 ) {
+//                                    Toast.makeText(FaceResult.this, "Oops! No face detected", Toast.LENGTH_LONG).show();
+                                    OpenDialog();
+                                }
+                                else if (faces.size() > 1) {
+                                    Toast.makeText(FaceResult.this, "More than one face detected", Toast.LENGTH_LONG).show();
+                                }
+                                else {
+                                    // There can be multiple faces detected from an image,
+                                    // manage them using loop from List<Face> faces
+                                    for(Face face: faces) {
+                                        // Get detected faces as rectangle
+                                        rect = face.getBoundingBox();
+                                        rect.set(rect.left * SCALING_FACTOR,
+                                                rect.top * (SCALING_FACTOR - 1),
+                                                rect.right * SCALING_FACTOR,
+                                                (rect.bottom * SCALING_FACTOR) + 90);
+                                    }
 
-                        // After detecting successfully, we need to disable the animation view
-                        ScanEffectView.setVisibility(View.GONE);
-                        Rect rect = null;
 
-                        if (faces.size() == 0 ) {
-//                            Toast.makeText(FaceResult.this, "Oops! No face detected", Toast.LENGTH_LONG).show();
-                            OpenDialog();
-                        }
-                        else if (faces.size() > 1) {
-                            Toast.makeText(FaceResult.this, "More than one face detected", Toast.LENGTH_LONG).show();
-                        }
-                        else {
-                            // There can be multiple faces detected from an image,
-                            // manage them using loop from List<Face> faces
-                            for(Face face: faces) {
-                                // Get detected faces as rectangle
-                                rect = face.getBoundingBox();
-                                rect.set(rect.left * SCALING_FACTOR,
-                                        rect.top * (SCALING_FACTOR - 1),
-                                        rect.right * SCALING_FACTOR,
-                                        (rect.bottom * SCALING_FACTOR) + 90);
+                                    // Draw a bounding box for detected face
+                                    Mat ProcessingMat = new Mat(); // Create a new mat
+                                    Utils.bitmapToMat(bitmap, ProcessingMat); // Convert the Bitmap image to Mat image
+                                    Imgproc.rectangle(
+                                            ProcessingMat,
+                                            new Point((float) rect.left, (float) rect.bottom),
+                                            new Point((float) rect.right, (float) rect.top),
+                                            new Scalar(255, 0, 0),
+                                            5
+                                    );
+
+                                    Bitmap ProcessedBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true); // Copy the original Bitmap to a mutable bitmap
+                                    Utils.matToBitmap(ProcessingMat, ProcessedBitmap); // Convert the Mat image to Bitmap image to display on image view
+
+                                    // Display the processed Bitmap
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            FaceResultView.setImageBitmap(ProcessedBitmap);
+                                        }
+                                    });
+                                }
                             }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Detection failed
+                                Toast.makeText(FaceResult.this, "Detection failed due to " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+//        DetectFaceThread.start();
 
-
-                            // Draw a bounding box for detected face
-                            ScanEffectView.setVisibility(View.GONE);
-
-                            Mat ProcessingMat = new Mat(); // Create a new mat
-                            Utils.bitmapToMat(bitmap, ProcessingMat); // Convert the Bitmap image to Mat image
-                            Imgproc.rectangle(
-                                    ProcessingMat,
-                                    new Point((float) rect.left, (float) rect.bottom),
-                                    new Point((float) rect.right, (float) rect.top),
-                                    new Scalar(255, 0, 0),
-                                    5
-                            );
-
-                            Bitmap ProcessedBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true); // Copy the original Bitmap to a mutable bitmap
-                            Utils.matToBitmap(ProcessingMat, ProcessedBitmap); // Convert the Mat image to Bitmap image to display on image view
-
-                            // Display the processed Bitmap
-                            FaceResultView.setImageBitmap(ProcessedBitmap);
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
+        Thread ConfirmAndRetakeThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BtnConfirm.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Detection failed
-                        Toast.makeText(FaceResult.this, "Detection failed due to " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    public void onClick(View v) {
+                        OpenHomeScreen();
                     }
                 });
+
+                BtnRetake.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        OpenCameraScreen();
+                    }
+                });
+            }
+        });
+//        ConfirmAndRetakeThread.start();
+
+        new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Toast.makeText(FaceResult.this, "We are detecting your face", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFinish() {
+                DetectFaceThread.start();
+                ConfirmAndRetakeThread.start();
+            }
+        }.start();
+    }
+
+    public void OpenHomeScreen() {
+        Intent HomeIntent = new Intent(this, MainActivity.class);
+        startActivity(HomeIntent);
+    }
+
+    public void OpenCameraScreen() {
+        Intent OpenCameraIntent = new Intent(this, OpenCamera.class);
+        startActivity(OpenCameraIntent);
     }
 
     public void OpenDialog() {
